fdf3b1264c2ca5e90635d7ae24b06561d658289f
CraftyRyte <9210@sanskritischool.edu.in>
2023-10-28 12:44:11 +0530

    Window Added
    Added a raylib window template to maximize production at start of Jam

--- main.cpp
+++ main.cpp
@@ -2,6 +2,13 @@
 #include "raylib-cpp.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+//    std::cout << "Hello, World!" << std::endl;
+    InitWindow(900, 900, "My Stuff Baby");
+    while (!WindowShouldClose()){
+        BeginDrawing();
+        EndDrawing();
+    }
+    CloseWindow();
+    exit(110000101100010011);
     return 0;
 }
