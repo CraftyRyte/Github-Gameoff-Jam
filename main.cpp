ee3e1fc911637b67516dd4a510cc7235396b3dca
CraftyRyte <9210@sanskritischool.edu.in>
2023-11-03 19:07:36 +0530

    Created Basic structure
    Created a basic structure of program using game_states to define the state of game (eg. Start, explore etc)

--- main.cpp
+++ main.cpp
@@ -2,13 +2,32 @@
 #include "raylib-cpp.hpp"
 
 int main() {
-//    std::cout << "Hello, World!" << std::endl;
     InitWindow(900, 900, "My Stuff Baby");
+    SetTargetFPS(60);
+
+    std::string game_states[4] = {"start", "menu", "explore", "fight"};
+    std::string game_state = game_states[2];
+
     while (!WindowShouldClose()){
-        BeginDrawing();
-        EndDrawing();
+        if (game_state == game_states[0]) {
+            BeginDrawing();
+            EndDrawing();
+        }
+        else if (game_state == game_states[1]) {
+            BeginDrawing();
+            EndDrawing();
+        }
+        else if (game_state == game_states[2]) {
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawFPS(10, 10);
+            EndDrawing();
+        }
+        else if (game_state == game_states[3]) {
+            BeginDrawing();
+            EndDrawing();
+        }
     }
     CloseWindow();
-    exit(110000101100010011);
     return 0;
 }
